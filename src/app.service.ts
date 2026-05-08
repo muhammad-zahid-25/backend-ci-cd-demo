2b0ff5d94109bdf6c8189c91020448ff392d7b3a
Muhammad Zahid <mzahid14cs25@gmail.com>
2026-05-09 00:18:09 +0500

    Test console added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('hello this is test purpose console');
     return 'Hello World!';
   }
 }
